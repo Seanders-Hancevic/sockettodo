937959475b1dc68e3f71a57c44b3ac302fc28b81
Seanders Hancevic <seanhance1@gmail.com>
2018-11-27 20:42:44 -0500

    ..

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 4000;
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+mongoose.Promise = global.Promise;
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://sh9669:password1234@ds227821.mlab.com:27821/heroku_zf4qqsff",
